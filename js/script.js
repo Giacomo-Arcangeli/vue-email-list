1ae7b7a5c525d56b2cbee3268b9135333e0856d9
Giacomo-Arcangeli <giacomoarcangeli1997@gmail.com>
2023-06-27 18:52:28 +0200

    email generator

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
+const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
+
 // Estrapolo il metodo createApp
 const { createApp } = Vue;
 
 // Inizializzo l'app Vue
 const app = createApp({
-    data() {
-        return {
-            
-        }
+  data() {
+    return {
+      emails: [],
+    };
+  },
+  created() {
+    for (let i = 0; i < 10; i++) {
+      axios.get(endpoint).then((res) => {
+        responseEmail = res.data.response;
+        this.emails.push(responseEmail);
+      });
     }
+  },
 });
 
 // La monto nell'elemento HTML "root"
-app.mount('#root');
+app.mount("#root");
